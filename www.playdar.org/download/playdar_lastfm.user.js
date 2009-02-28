016a12bb5a9c5c5ea552553be5c4bcc83fbc3706
James Wheare <james@wheare.org>
2009-03-01 06:41:09 +0800

    Hot regexicide action
    Signed-off-by: Richard Jones <rj@metabrew.com>

--- www.playdar.org/download/playdar_lastfm.user.js
+++ www.playdar.org/download/playdar_lastfm.user.js
@@ -82,12 +82,12 @@ function resolve_links (playdar, results_handler) {
     var links = unsafeWindow.$$('a');
     for (var i = 0; i < links.length; i++) {
         // Only match links in the /music path
-        var url = parseUri(links[i].href);
-        if (url.directoryPath.substr(0, 7) != "/music/") {
+        var path = links[i].pathname;
+        if (path.substr(0, 7) != "/music/") {
             continue;
         }
         // Only match track links
-        var urlparts = new RegExp(/^\/music\/(.+)\/_\/(.+)/).exec(url.path);
+        var urlparts = new RegExp(/^\/music\/(.+)\/_\/(.+)/).exec(path);
         if (!urlparts) {
             continue;
         }
@@ -117,37 +117,4 @@ function resolve (playdar, link, artist, track, results_handler) {
     // register results handler and resolve for this qid
     playdar.register_results_handler(results_handler, qid);
     playdar.resolve(artist, "", track, qid);
-}
-
-/* parseUri JS v0.1, by Steven Levithan (http://badassery.blogspot.com)
-Splits any well-formed URI into the following parts (all are optional):
-----------------------
-• source (since the exec() method returns backreference 0 [i.e., the entire match] as key 0, we might as well use it)
-• protocol (scheme)
-• authority (includes both the domain and port)
-    • domain (part of the authority; can be an IP address)
-    • port (part of the authority)
-• path (includes both the directory path and filename)
-    • directoryPath (part of the path; supports directories with periods, and without a trailing backslash)
-    • fileName (part of the path)
-• query (does not include the leading question mark)
-• anchor (fragment)
-*/
-
-function parseUri(sourceUri) {
-    var uriPartNames = ["source","protocol","authority","domain","port","path","directoryPath","fileName","query","anchor"];
-    var uriParts = new RegExp("^(?:([^:/?#.]+):)?(?://)?(([^:/?#]*)(?::(\\d*))?)?((/(?:[^?#](?![^?#/]*\\.[^?#/.]+(?:[\\?#]|$)))*/?)?([^?#/]*))?(?:\\?([^#]*))?(?:#(.*))?").exec(sourceUri);
-    var uri = {};
-    
-    for(var i = 0; i < 10; i++){
-        uri[uriPartNames[i]] = (uriParts[i] ? uriParts[i] : "");
-    }
-    
-    // Always end directoryPath with a trailing backslash if a path was present in the source URI
-    // Note that a trailing backslash is NOT automatically inserted within or appended to the "path" key
-    if(uri.directoryPath.length > 0){
-        uri.directoryPath = uri.directoryPath.replace(/\/?$/, "/");
-    }
-    
-    return uri;
 }
