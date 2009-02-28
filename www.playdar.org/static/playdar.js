8c8c0068de44fa5540f23cd2db3f243e382c7724
James Wheare <james@wheare.org>
2009-02-28 10:33:40 +0800

    playdar.js 0.2
    Signed-off-by: Richard Jones <rj@metabrew.com>

--- www.playdar.org/static/playdar.js
+++ www.playdar.org/static/playdar.js
@@ -1,115 +1,196 @@
-function Playdar() {
-    // CALLBACKS YOU SHOULD OVERRIDE:
-    this.cb_detected = function(v) { alert('Playdar detected, version: '+v); }
-    this.cb_not_detected = function() { alert('Playdar not detected'); }
-    this.cb_results_available = function(r,finalanswer) { alert('Results finalanswer: '+finalanswer+' (JSON object): ' + r); }
-    // END CALLBACKS
-
-    this.custom_results_handlers = new Array();
-    this.qidpoll = new Array();
-    this.stat = false;
-    this.last_qid="";
-    this.libver = "0.1"; // version of playdar.js
-
-    this.init = function() {
-        this.stat();
+Playdar = function (handlers) {
+    if (handlers) {
+        for (handler in handlers) {
+            this.register_handler(handler, handlers[handler]);
+        }
     }
+    this.uuid = Playdar.generate_uuid();
+    Playdar.instances[this.uuid] = this;
+};
 
-    this.httpbase = function(){
-        return "http://localhost:8888"; 
-    }
+Playdar.instances = {};
+Playdar.generate_uuid = function () {
+    return "playdar_js_uuid_gen_" + Math.random(); // TODO improve.
+};
 
-    // api base url for playdar requests
-    this.url = function(u){ 
-        return this.httpbase() + "/api/?" + u; 
+// format secs -> mm:ss helper.
+Playdar.mmss = function (secs) {
+    var s = secs % 60;
+    if (s < 10) {
+        s = "0" + s;
     }
-
-    // turn a source id into an url to stream from
-    this.sid2url = function(sid){
-        return this.httpbase() + "/sid/" + sid;
-    }
-
-    // is playdar installed?
-    this.stat = function() {
-        setTimeout('playdar.stat_timeout()', 2000);
-        this.loadjs(this.url("method=stat&jsonp=playdar.stat_cb"));
-    }
+    return Math.floor(secs/60) + ":" + s;
+};
     
-    this.set_qid_results_callback = function(qid, fun) {
-        this.custom_results_handlers[qid]=fun;
-    }
+Playdar.loadjs = function (url) {
+   var e = document.createElement("script");
+   e.src = url;
+   document.getElementsByTagName("head")[0].appendChild(e);
+};
 
-    this.stat_timeout = function() { 
-        if(this.stat.name != "playdar") this.cb_not_detected();
-    }
-
-    this.stat_cb = function(response) {
-        if(response.name != "playdar") return false;
-        this.stat = response;    
-        this.cb_detected(this.stat.version);
-    }
-
-    this.resolve_callback = function(response){
-        this.last_qid = response.qid;
-        this.check_qid(response.qid);
-    }
-
-    this.check_qid = function(qid){
-        var checkurl = this.url("method=get_results&qid=" + qid + "&jsonp=playdar.search_results");
-        this.loadjs(checkurl);
-    }
-
-    this.search_results = function(response){
-        var finalanswer=false;
+Playdar.prototype = {
+    lib_version: "0.2",
+    server_root: "localhost",
+    server_port: "8888",
+    stat_timeout: 2000,
+    
+    register_handler: function (handler_name, callback) {
+        this.handlers[handler_name] = callback;
+    },
+    
+    // CALLBACKS YOU SHOULD OVERRIDE:
+    handlers: {
+        detected: function (v) {
+            alert('Playdar detected, version: ' + v);
+        },
+        not_detected: function () {
+            alert('Playdar not detected');
+        },
+        results: function (r, final_answer) {
+            alert('Results final answer: ' + final_answer + ' (JSON object): ' + r);
+        }
+    },
+    // END CALLBACKS
+    
+    init: function () {
+        this.stat();
+    },
+    
+    get_base_url: function (path) {
+        var url = "http://" + this.server_root + ":" + this.server_port;
+        if (path) {
+            url += path;
+        }
+        return url;
+    },
+    
+    // api base url for playdar requests
+    get_url: function (u) {
+        return this.get_base_url("/api/?" + u);
+    },
+    
+    jsonp: function (callback) {
+        return "&jsonp=Playdar.instances['" + this.uuid + "']." + callback;
+    },
+    
+    // turn a source id into a stream url
+    get_stream_url: function (sid) {
+        return this.get_base_url("/sid/" + sid);
+    },
+    
+    
+    // STAT PLAYDAR INSTALLATION
+    
+    stat_response: false,
+    check_stat: function () {
+        return this.stat_response && this.stat_response.name == "playdar";
+    },
+    
+    stat_timeout: function () {
+        if (!this.check_stat()) {
+            this.handlers.not_detected();
+        }
+    },
+    
+    stat_handler: function (response) {
+        this.stat_response = response;
+        if (!this.check_stat()) {
+            this.stat_response = false;
+            return false;
+        }
+        this.handlers.detected(this.stat_response.version);
+    },
+    
+    stat: function () {
+        var that = this;
+        setTimeout(function () {
+            that.stat_timeout();
+        }, this.stat_timeout);
+        var url = "method=stat"
+                + this.jsonp("stat_handler");
+        Playdar.loadjs(this.get_url(url));
+    },
+    
+    
+    // SEARCH RESULT POLLING
+    
+    // set search result handlers for a query id
+    results_handlers: {},
+    register_results_handler: function (qid, handler) {
+        this.results_handlers[qid] = handler;
+    },
+    
+    poll_counts: {},
+    should_stop_polling: function (response) {
+        // Stop if we've exceeded our refresh limit
+        if (response.refresh_interval <= 0) {
+            return true;
+        }
+        // Stop if the query is solved
+        if (response.query.solved == true) {
+            return true;
+        }
+        // Stop if we've got a perfect match
+        if (response.length > 0 && response.results[0].score == 1.0) {
+            return true;
+        }
+        // Stop if we've exceeded 4 polls
+        if (!this.poll_counts[response.qid]) {
+            this.poll_counts[response.qid] = 0;
+        }
+        if (++this.poll_counts[response.qid] >= 4) {
+            return true;
+        }
+        return false;
+    },
+    
+    handle_results: function (response) {
         // figure out if we should re-poll, or if the query is solved/failed:
-        if( response.query.solved == true ) finalanswer=true;
-        if( response.refresh_interval > 0 &&
-            !(response.length > 0 && response.results[0].score == 1.0) ){
-            if(!this.qidpoll[response.qid]) this.qidpoll[response.qid]=0;
-            if(++this.qidpoll[response.qid] < 4){
-                setTimeout('playdar.check_qid("'+response.qid+'")', response.refresh_interval);
-            }else{
-                finalanswer = true; // we won't poll again, tried enough times already
-            }
+        var final_answer = this.should_stop_polling(response);
+        if (!final_answer) {
+            var that = this;
+            setTimeout(function () {
+                that.poll_results(response.qid);
+            }, response.refresh_interval);
         }
-        // now call the results callback:
-        if(response.qid && this.custom_results_handlers[response.qid]){
-            // custom handler for this specific qid:
-            this.custom_results_handlers[response.qid](response, finalanswer);
-        }else{
-            // standard callback used:
-            this.cb_results_available(response, finalanswer);
+        // now call the results handler
+        if (response.qid && this.results_handlers[response.qid]) {
+            // try a custom handler registered for this query id
+            this.results_handlers[response.qid](response, final_answer);
+        } else {
+            // fall back to standard handler
+            this.handlers.results(response, final_answer);
         }
-    }
-
-    this.generate_uuid = function() {
-        return "playdar_js_uuid_gen_"+Math.random(); // TODO improve.
-    }
+    },
+    
+    // poll results for a query id
+    poll_results: function (qid) {
+        var url = "method=get_results"
+                + "&qid=qid"
+                + this.jsonp("handle_results");
+        var checkurl = this.get_url(url);
+        Playdar.loadjs(checkurl);
+    },
+    
+    
+    // CONTENT RESOLUTION
+    
+    last_qid: "",
+    handle_resolution: function (response) {
+        this.last_qid = response.qid;
+        this.poll_results(response.qid);
+    },
     
     // narrow, find an exact match, hopefully
-    this.doresolve = function(art,alb,trk,qid){
+    resolve: function (art, alb, trk, qid) {
         var url = "method=resolve";
-        url += "&jsonp=playdar.resolve_callback";
-        url += "&artist=" + art;
-        url += "&album=" + alb;
-        url += "&track=" + trk;
-        if(typeof(qid)!='undefined') url+="&qid="+qid;
-        this.loadjs(this.url(url));
+                + "&artist=" + art;
+                + "&album=" + alb;
+                + "&track=" + trk;
+                + this.jsonp("handle_resolution");
+        if (typeof(qid) != 'undefined') {
+            url += "&qid=" + qid;
+        }
+        Playdar.loadjs(this.get_url(url));
     }
-
-    this.loadjs = function(url){
-       var e = document.createElement("script");
-       e.src = url;
-       e.type="text/javascript";
-       document.getElementsByTagName("head")[0].appendChild(e); 
-    }
-
-    // format secs -> mm:ss helper.
-    this.mmss = function(secs) {
-        var s = secs%60;
-        if(s < 10) s="0"+s;
-        return Math.floor(secs/60) + ":" + s;
-    }
-} // end Playdar class
-
-var playdar = new Playdar();
+};
