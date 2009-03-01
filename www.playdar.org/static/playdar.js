b1ff46ad461f99da3fba429d2232af56625c3c59
James Wheare <james@wheare.org>
2009-03-01 08:01:45 +0800

    Nicer UUID generation from http://www.broofa.com/Tools/Math.uuid.js
    Signed-off-by: Richard Jones <rj@metabrew.com>

--- www.playdar.org/static/playdar.js
+++ www.playdar.org/static/playdar.js
@@ -15,8 +15,54 @@ Playdar.create = function (handlers) {
 
 Playdar.last = null;
 Playdar.instances = {};
-Playdar.generate_uuid = function () {
-    return "playdar_js_uuid_gen_" + Math.random(); // TODO improve.
+/*
+Based on: Math.uuid.js
+Version: 1.3
+Latest version:   http://www.broofa.com/Tools/Math.uuid.js
+Information:      http://www.broofa.com/blog/?p=151
+Contact:          robert@broofa.com
+----
+    Copyright (c) 2008, Robert Kieffer
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Robert Kieffer nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+Playdar.generate_uuid = function (len, radix) {
+    // Private array of chars to use
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''); 
+    var uuid = [];
+    var rnd = Math.random;
+    radix = radix || chars.length;
+    
+    if (len) {
+        // Compact form
+        for (var i = 0; i < len; i++) {
+            uuid[i] = chars[0 | rnd() * radix];
+        }
+    } else {
+        // rfc4122, version 4 form
+        var r;
+        
+        // rfc4122 requires these characters
+        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+        uuid[14] = '4';
+        
+        // Fill in random data.  At i==19 set the high bits of clock sequence as
+        // per rfc4122, sec. 4.1.5
+        for (var i = 0; i < 36; i++) {
+            if (!uuid[i]) {
+                r = 0 | rnd()*16;
+                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
+            }
+        }
+    }
+    return uuid.join('');
 };
 
 Playdar.toQueryString = function (params) {
