21c798bb967c3fcad806d2de952a672257619a26
James Wheare <james@wheare.org>
2009-03-01 08:05:01 +0800

    Don't need half this stuff
    Signed-off-by: Richard Jones <rj@metabrew.com>

--- www.playdar.org/static/playdar.js
+++ www.playdar.org/static/playdar.js
@@ -33,33 +33,25 @@ Redistribution and use in source and binary forms, with or without modification,
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-Playdar.generate_uuid = function (len, radix) {
+Playdar.generate_uuid = function () {
     // Private array of chars to use
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''); 
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
     var uuid = [];
     var rnd = Math.random;
-    radix = radix || chars.length;
     
-    if (len) {
-        // Compact form
-        for (var i = 0; i < len; i++) {
-            uuid[i] = chars[0 | rnd() * radix];
-        }
-    } else {
-        // rfc4122, version 4 form
-        var r;
-        
-        // rfc4122 requires these characters
-        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-        uuid[14] = '4';
-        
-        // Fill in random data.  At i==19 set the high bits of clock sequence as
-        // per rfc4122, sec. 4.1.5
-        for (var i = 0; i < 36; i++) {
-            if (!uuid[i]) {
-                r = 0 | rnd()*16;
-                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
-            }
+    // rfc4122, version 4 form
+    var r;
+    
+    // rfc4122 requires these characters
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    uuid[14] = '4';
+    
+    // Fill in random data.  At i==19 set the high bits of clock sequence as
+    // per rfc4122, sec. 4.1.5
+    for (var i = 0; i < 36; i++) {
+        if (!uuid[i]) {
+            r = 0 | rnd()*16;
+            uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
         }
     }
     return uuid.join('');
