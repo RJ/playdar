a07425ff58e66644c8f31ae3a887b1758ad328e5
Max Howell <mxcl@arya.lan>
2009-03-02 10:48:35 +0800

    playdar.prefPane, initial commit. Needs a little more work, but the playdar inside is functional. Intel only. May require OS X 10.4.
    Signed-off-by: Richard Jones <rj@metabrew.com>

---
+++ playdar-prefpane/main.h
@@ -0,0 +1,44 @@
+/*
+ Created on 28/02/2009
+ Copyright 2009 Max Howell <mxcl@users.sf.net>
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#import <PreferencePanes/PreferencePanes.h>
+
+// long class name is because we get loaded into System Preferences process, so 
+// we are required to be ultra verbose
+// http://developer.apple.com/documentation/UserExperience/Conceptual/PreferencePanes/Tasks/Conflicts.html#//apple_ref/doc/uid/20000706
+@interface OrgPlaydarPreferencePane : NSPreferencePane 
+{
+    IBOutlet NSPopUpButton* popup;
+    IBOutlet NSButton* scan;
+    IBOutlet NSButton* start;
+}
+
+-(void)mainViewDidLoad;
+
+-(void)openPanelDidEnd:(NSOpenPanel*)panel
+            returnCode:(int)returnCode
+           contextInfo:(void*)contextInfo;
+
+-(void)exec:(NSString*)command
+   withArgs:(NSArray*)args;
+
+-(IBAction)select:(id)sender; //TODO doesn't need to be IBAction
+-(IBAction)onScan:(id)sender;
+-(IBAction)onStart:(id)sender;
+
+@end
---
+++ playdar-prefpane/pc.h
@@ -0,0 +1,8 @@
+//
+// Prefix header for all source files of the 'playdar-prefpane' target in the 'playdar-prefpane' project.
+//
+
+#ifdef __OBJC__
+    #import <Foundation/Foundation.h>
+    #import <AppKit/AppKit.h>
+#endif
