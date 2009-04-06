d3a8f7ca570726b0cf0ccc8f8f5c4faf16bd4d64
dougma <doug.mansell@gmail.com>
2009-04-06 18:53:42 +0100

    boffin plugin (work in progress)

--- resolvers/boffin/Boffin.cpp
+++ resolvers/boffin/Boffin.cpp
@@ -2,6 +2,8 @@
 
 
 Boffin::Boffin()
+    : m_thread( 0 )
+    , m_thread_stop( false )
 {
 }
 
@@ -11,10 +13,85 @@ Boffin::name() const
     return "Boffin";
 }
 
+// return false to disable resolver
+bool 
+Boffin::init(playdar::Config* c, Resolver* r)
+{
+    if (!ResolverServicePlugin::init(c, r))
+        return false;
+
+    m_thread = new boost::thread( boost::bind(&Boffin::thread_run, this) );
+    return true;
+}
+
+void
+Boffin::queue_work(boost::function< void() > work)
+{
+    if (!m_thread_stop) 
+    {
+        {
+            boost::lock_guard<boost::mutex> guard(m_queue_mutex);
+            m_queue.push(work);
+        }
+        m_queue_wake.notify_one();
+    }
+}
+
+boost::function< void() > 
+Boffin::get_work()
+{
+    boost::unique_lock<boost::mutex> lock(m_queue_mutex);
+    while (m_queue.empty()) {
+        m_queue_wake.wait(lock);
+    }
+    boost::function< void() > result = m_queue.front();
+    m_queue.pop();
+    return result;
+}
+
+void
+Boffin::drain_queue()
+{
+    boost::lock_guard<boost::mutex> guard(m_queue_mutex);
+    while (!m_queue.empty()) {
+        m_queue.pop();
+    }
+}
+
+void
+Boffin::thread_run()
+{
+    try {
+        while (!m_thread_stop) {
+            get_work()();
+        }
+    } catch (boost::thread_interrupted) {
+        std::cout << "Boffin::thread_run exiting normally";
+    } catch (...) {
+        std::cout << "Boffin::thread_run unhandled exception";
+    }
+
+    drain_queue();
+}
+
+void
+Boffin::stop()
+{
+    if (m_thread) {
+        m_thread_stop = true;
+        m_thread->interrupt();
+        m_thread->join();
+        delete m_thread;
+        m_thread = 0;
+    }
+}
+
 /// max time in milliseconds we'd expect to have results in.
 unsigned int 
 Boffin::target_time() const
 {
+    // we may have zero results (if it's a query we can't handle), 
+    // and we don't want to delay any other resolvers, so:
     return 50;
 }
 
@@ -22,22 +99,21 @@ Boffin::target_time() const
 unsigned short 
 Boffin::weight() const
 {
-    return 1000;
+    return 80;
 }
 
 void 
 Boffin::start_resolving(boost::shared_ptr<ResolverQuery> rq)
 {
+    queue_work( boost::bind( &Boffin::resolve, this, rq ) );
 }
 
-/** thread-safe */
-bool 
-Boffin::report_results(query_uid qid, 
-                       vector< boost::shared_ptr<PlayableItem> > results, 
-                       string via)
+void
+Boffin::resolve(boost::shared_ptr<ResolverQuery> rq)
 {
-    return false;
+    int i = 0;
 }
+
 
 // default is empty, ie no http urls handle
 vector<string> 
--- resolvers/boffin/Boffin.h
+++ resolvers/boffin/Boffin.h
@@ -2,12 +2,18 @@
 #define __RS_BOFFIN_H__
 
 #include "playdar/playdar_plugin_include.h"
+#include <queue>
+#include <boost/function.hpp>
+#include <boost/thread.hpp>
 
 
-class Boffin : public ResolverService
+class Boffin : public ResolverServicePlugin
 {
 public:
     Boffin();
+
+    // return false to disable the plugin
+    virtual bool init(playdar::Config*, Resolver*);
    
     virtual std::string name() const;
     
@@ -19,11 +25,6 @@ public:
 
     virtual void start_resolving(boost::shared_ptr<ResolverQuery> rq);
 
-    /** thread-safe */
-    virtual bool report_results(query_uid qid, 
-        vector< boost::shared_ptr<PlayableItem> > results,
-        string via);
-    
     // default is empty, ie no http urls handle
     virtual vector<string> get_http_handlers();
 
@@ -34,6 +35,24 @@ public:
                                  map<string,string> postvars,
                                 playdar::auth * pauth);
 
+private:
+    void resolve(boost::shared_ptr<ResolverQuery> rq);
+
+    void thread_run();
+    void queue_work(boost::function< void() > work);
+    boost::function< void() > get_work();
+    void drain_queue();
+    void stop();
+
+    volatile bool m_thread_stop;
+    boost::thread* m_thread;
+    boost::mutex m_queue_mutex;
+    boost::condition_variable m_queue_wake;
+    std::queue< boost::function< void() > > m_queue;
 };
 
+EXPORT_DYNAMIC_CLASS( Boffin )
+
+
 #endif
+
