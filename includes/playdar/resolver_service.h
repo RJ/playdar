96f479d70b72b242e26d332a9d82ebbf8698efa1
nova77 <nova77@gmail.com>
2009-04-18 16:18:50 +0100

    first version of the plugin interface, also a slimming diet for the whole interface

--- includes/playdar/resolver_service.h
+++ includes/playdar/resolver_service.h
@@ -1,25 +1,36 @@
 #ifndef __RESOLVER_SERVICE_H__
 #define __RESOLVER_SERVICE_H__
 // Interface for all resolver services
-#include "playdar/application.h"
-#include "playdar/resolver.h"
-#include "playdar/auth.hpp"
-#include "playdar/playdar_response.h"
+
+//#include "playdar/application.h"
+//#include "playdar/resolver.h"
+//#include "playdar/auth.hpp"
+
 
 #include <DynamicClass.hpp>
+#include <iostream> // that HAS to be removed!
+#include <string>
+
+#include "playdar/pluginadaptor.h"
+#include "playdar/playdar_response.h"
+
+#include "json_spirit/json_spirit.h"
+#include "playdar/types.h"
 
 namespace playdar {
 
 class playdar_request;
+class auth;
+
 class ResolverService
 {
 public:
+    /// Constructor
     ResolverService(){}
+    // destructor is protected, see below
     
     virtual void Destroy()
-    {
-        cout << "Unloading " << name() << endl;
-    }
+    { std::cout << "Unloading " << name() << std::endl; }
     
     /// called once at startup. returning false disables this resolver.
     virtual bool init(pa_ptr pap) = 0;
@@ -28,15 +39,11 @@ public:
     
     /// max time in milliseconds we'd expect to have results in.
     virtual unsigned int target_time() const
-    {
-        return 1000;
-    }
+    { return 1000; }
     
     /// highest weighted resolverservices are queried first.
     virtual unsigned short weight() const
-    {
-        return 100;
-    }
+    { return 100; }
 
     /// start searching for matches for this query
     virtual void start_resolving(boost::shared_ptr<ResolverQuery> rq) = 0;
@@ -46,16 +53,16 @@ public:
     virtual void cancel_query(query_uid qid){ /* no-op */ }
 
     /// handler for HTTP reqs we are registered for:
-    virtual playdar_response http_handler( const playdar_request&, playdar::auth * pauth)
-    {
-        return "This plugin has no web interface.";
-    }
+    virtual playdar_response http_handler(const playdar_request*, auth* pauth)
+    { return "This plugin has no web interface."; }
     
 protected:
     
     virtual ~ResolverService() throw() {  }
-
 };
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 /// this is what the dynamically loaded resolver plugins extend:
 class ResolverServicePlugin 
@@ -63,8 +70,34 @@ class ResolverServicePlugin
    public ResolverService
 {
 public:
+
+    /// Cloning method, this will have to return a new object of the same
+    /// derived type
+    virtual ResolverService* create( PluginAdaptor* pPA ) = 0;
+
+public:
     DECLARE_DYNAMIC_CLASS( ResolverServicePlugin )
+
+protected:
+
+    template <typename T>
+    struct custom_deleter
+    {
+        void operator() (T *pObj)
+        { delete pObj; }
+    };
+
+protected:
+
+    template <typename T>
+    boost::shared_ptr<T> object_factory()
+    { 
+        return boost::shared_ptr<T>( new T(), custom_deleter<T>() );
+    }
+
 };
+
+////////////////////////////////////////////////////////////////////////////////
 
 }
 
