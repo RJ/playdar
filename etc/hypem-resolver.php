5ad4dc4ea342ad84e60d0bf9b2950d8faedc51bb
David Singleton <davidsingleton@david-3.office.last.fm>
2009-04-21 17:30:45 +0100

    Commit working state of hypem resolver, needs a cleanup, especially the cookie handling.

--- etc/hypem-resolver.php
+++ etc/hypem-resolver.php
@@ -1,39 +1,85 @@
+#!/usr/local/php5/bin/php
 <?php
 
 require_once dirname(__FILE__) . '/phpresolver/playdarresolver.php';
 
+/**
+  * A resolver for MP3s from blogs indexed by Hype Machine
+  * @author David Singleton (http://dsingleton.co.uk)
+  * @todo Get the cookie once only, cache and re-send.
+  */
 class HypeMachineResolver extends PlaydarResolver
 {
     protected $name = 'HypeMachine Resolver';
-    protected $targetTime = 150; // Rough guess for a remote service
-    protected $weight = 70; 
+    protected $targetTime = 5000; // Rough guess for a remote service
+    protected $weight = 60;
+    
+    private $cookie = array();
     
     public function resolve($request)
     {
         $source = sprintf('http://hypem.com/search/"%s"+"%s"/', urlencode($request->artist), urlencode($request->track));
-        
+        // $html = file_get_contents("test/hypem.html");
         // We need to grab the AUTH cookie we get issued here.
-        $html = file_get_contents($source);
+        $ch = curl_init();
         
+        curl_setopt($ch, CURLOPT_URL, $source);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Playdar 0.1');
+        
+        $x = curl_exec($ch);
+        curl_close($ch);
+        
+        if (!$x) {
+            // you are offline...
+            return array();
+        }   
+        
+        $headers = array();
+        
+        list($head, $html) = explode("\r\n\r\n", $x, 2);
+        
+        $aHeaders = explode("\r\n", $head);
+        array_shift($aHeaders);
+        
+        foreach($aHeaders as $header) {
+            list($key, $val) = explode(": ", $header);
+            $headers[$key] = $val;
+        }
+        list($cookies) = explode(";", $headers['Set-Cookie']);
+        list($key, $val) = explode("=", $cookies);
+        $aCookies = array($key => $val);
+        
+        $auth = $aCookies['AUTH'];
+        // $auth = 'XXXX';
+
         preg_match_all('/\({.*?}\)/s', $html, $matches);
-        
         $results = array();
-        
         foreach($matches[0] as $match) {
             // We might want to filter out some of the fields, they're mostly useless.
             $json = $this->reformatHypeJSON(substr($match, 2, -2));
             $result = json_decode($json);
-            
+            if (!$result) {
+                continue;
+            }
             $result->score = $this->calculateRelevancy($request, $result);
             $result->url = sprintf("http://hypem.com/serve/play/%s/%s", $result->id, $result->key);
             $result->track = $result->song;
+
+            $result->duration = (int) $result->time;
+            $result->extra_headers = array("Cookie: AUTH=$auth");
+            $result->source = "Hype Machine";
             // Use the unique id as a fraction to vary keys where the score is the same.
             $key =  round($result->score) . "-{$result->id}";
             
+            $result->bitrate = 128;
             $results[$key] = $result;
         }
         
         krsort($results, SORT_NUMERIC);
+        $results = array_values(array_slice($results, 0, 2));
+        
         return $results;
     }
     
@@ -71,14 +117,7 @@ class HypeMachineResolver extends PlaydarResolver
         }
 
         // Out of 100
-        return ($artistSimilarity + $trackSimilarity) / 2;
-    }
-    
-    function proxy($url)
-    {
-        // get cookie val
-        // make request
-        // passthru not download
+        return ($artistSimilarity + $trackSimilarity) / 200;
     }
 }
 
