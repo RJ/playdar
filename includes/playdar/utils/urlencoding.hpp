35c31ed070e25e598dcd7f8c07da7547ed4c3947
Jono Cole <jonocole@gmail.com>
2009-04-23 16:42:36 +0100

    fix url decoding of + chars

--- includes/playdar/utils/urlencoding.hpp
+++ includes/playdar/utils/urlencoding.hpp
@@ -2,6 +2,7 @@
 #define _PLAYDAR_UTILS_URLENCODING_H_
 
 #include <curl/curl.h>
+#include <boost/algorithm/string.hpp>
 
 namespace playdar { namespace utils {
     std::string url_encode( const std::string & s )
@@ -17,7 +18,7 @@ namespace playdar { namespace utils {
         char* c = curl_easy_unescape( 0, s.c_str(), 0, 0 );
         const std::string& ret = c;
         curl_free( c );
-        return ret;
+        return boost::replace_all_copy( ret, "+", " " );;
     }
 }}
 
