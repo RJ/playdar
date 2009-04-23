cf37c00612ee20cd07bb30ff3427a9adf9af328a
dougma <doug.mansell@gmail.com>
2009-04-23 11:54:14 +0100

    fix uninitialised m_streaming property in stock_reply

--- deps/moost_http/src/http/reply.cpp
+++ deps/moost_http/src/http/reply.cpp
@@ -244,6 +244,7 @@ reply reply::stock_reply(reply::status_type status)
   rep.headers[0].value = boost::lexical_cast<std::string>(rep.content.size());
   rep.headers[1].name = "Content-Type";
   rep.headers[1].value = "text/html";
+  rep.m_streaming = false;
   return rep;
 }
 
