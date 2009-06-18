3e4dcf71e712b82e88f82138cbd041f5e7a2ee2f
dougma <doug.mansell@gmail.com>
2009-06-18 18:27:28 +0100

    use tab separators on TRACK: and TAG: lines

--- resolvers/local/scanner/scanner.cpp
+++ resolvers/local/scanner/scanner.cpp
@@ -50,25 +50,28 @@ Library *gLibrary;
 
 int scanned, skipped, ignored = 0;
 
-#if 0   //deadcode
-string url_encode( const string& p )
+
+// replace whitespace and other control codes with ' ' and replace multiple whitespace with single
+string 
+fixspaces(const string& s)
 {
-    // url encode everything between '/' character
-    vector<std::string> v;
-    split(v, p, boost::is_any_of( "/" ));
-    string ret = "";
-    bool first = true;
-    BOOST_FOREACH(const string& s, v) {
-        if (first) {
-            first = false;
+    string r;
+    bool prevWasSpace = false;
+    r.reserve(s.length());
+    for (string::const_iterator i = s.begin(); i != s.end(); i++) {
+        if (*i > 0 && *i <= ' ') {
+            if (!prevWasSpace) {
+                r += ' ';
+                prevWasSpace = true;
+            }
         } else {
-            ret += "/";
+            r += *i;
+            prevWasSpace = false;
         }
-        ret += playdar::utils::url_encode(s);
     }
-    return ret;
+    return r;
 }
-#endif
+
 
 string urlify(const string& p)
 {
@@ -103,7 +106,7 @@ bool scan(const Path& p, map<string, int>& mtimes)
         DirIt end_itr;
         for(DirIt itr( p ); itr != end_itr; ++itr){
             if ( bfs::is_directory( itr->status() ) ){
-                cout << "DIR: " << toUtf8(itr->path().string()) << endl;
+                cout << "DIR:\t" << toUtf8(itr->path().string()) << endl;
                 scan(itr->path(), mtimes);
             } else {
                 // is this file an audio file we understand?
@@ -132,7 +135,9 @@ bool scan(const Path& p, map<string, int>& mtimes)
             }
         }
     }
-    catch(std::exception const& e) { cerr << e.what() << endl;}
+    catch(std::exception const& e) { 
+        cerr << e.what() << endl;
+    }
     //catch(...) { cout << "Fail at " << p.string() << endl; }
 
     return true;
@@ -175,6 +180,7 @@ bool add_file(const Path& p, int mtime)
         boost::trim(album);
         boost::trim(track);
         if (artist.length()==0 || track.length()==0) {
+            cout << "NOTAGS:\t" << toUtf8(p.string()) << endl;
             ignored++;
             return false;
         }
@@ -198,18 +204,11 @@ bool add_file(const Path& p, int mtime)
                             track, 
                             tag->track() );
 
-       /* cout << "-- TAG --" << endl;
-        cout << "title   - \"" << tag->title()   << "\"" << endl;
-        cout << "artist  - \"" << tag->artist()  << "\"" << endl;
-        cout << "album   - \"" << tag->album()   << "\"" << endl;
-        cout << "year    - \"" << tag->year()    << "\"" << endl;
-        cout << "comment - \"" << tag->comment() << "\"" << endl;
-        cout << "track   - \"" << tag->track()   << "\"" << endl;
-        cout << "genre   - \"" << tag->genre()   << "\"" << endl;*/
-        cout << "T " 
-             << tag->artist() << "\t"
-             << tag->album() << "\t"
-             << tag->title() << "\t" << endl;
+        // fixspaces ensures the field separation doesn't get messed up
+        cout << "TRACK:\t" 
+             << fixspaces(artist) << "\t" 
+             << fixspaces(album)  << "\t" 
+             << fixspaces(track)  << endl;
 
         scanned++;
         return true;
